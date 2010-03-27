44596b7b764a854ee0d1498fcff7c8d2ac2288ed
Ed Gauthier <ed@egauthier.net>
2010-03-26 20:07:03 -0400

    Added ability to specify hash algorithm (sha1|md5)

--- dirHash.py
+++ dirHash.py
@@ -5,7 +5,7 @@ import os
 import hashlib
 
 def usage():
-  print "Usage: dirHash.py <source>"
+  print "Usage: dirHash.py <source> [sha1|md5]"
 
 
 def main(args):
@@ -13,28 +13,37 @@ def main(args):
   # get source
   source = args[0]
 
+  # determine hashing algorithm
+  hash = 'sha1'
+  if len(args) > 1:
+    hash = args[1]
+
+  if not hash in ['md5','sha1']:
+    usage()
+    return
+
   # make sure source exists
   if not os.path.exists(source):
     print "Source doesn't exist: %s" % source
 
   # calculate hashes for all files in the source
-  hashDir(source)
+  hashDir(source,hash)
 
 
-def hashDir(source):
+def hashDir(source,hash):
   for base, dirs, files in os.walk(source):
     for file in files:
       fileName = os.path.join(base, file)
-      print "%s|%s" % (getHashForFile(fileName),fileName)
+      print "%s|%s|%s" % (hash,getHashForFile(fileName,hash),fileName)
 
 
-def getHashForFile(fileName, blockSize=2**8):
+def getHashForFile(fileName, hash='sha1', blockSize=2**8):
   try:
     f = open(fileName,'rb')
   except:
     return "  *******  Error opening file! *******  "
   else:
-    h = hashlib.sha1()
+    h = hashlib.new(hash)
     while True:
       data = f.read(blockSize)
       if not data:
@@ -45,7 +54,7 @@ def getHashForFile(fileName, blockSize=2**8):
 
 
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
+  if len(sys.argv) < 2:
     usage()
     sys.exit(2)
 
