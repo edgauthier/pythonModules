480d5ae8ca96d87b5df81510c0de6080c639eec0
Ed Gauthier <ed@egauthier.net>
2011-11-05 11:36:25 -0400

    Updates for pinboard export script
    - Add support for exporting to a specific directory.
    - Better handling of entered formats.
    - Better usage output.

--- pinboardExport.py
+++ pinboardExport.py
@@ -6,6 +6,7 @@ import urllib
 import urllib2
 import cookielib
 import getpass
+import os
 
 class PinboardExporter(object):
     """Logs into pinboard.in and exports bookmarks in various formats."""
@@ -14,11 +15,11 @@ class PinboardExporter(object):
         self.password = password
         self._connect()
     
-    def export_bookmarks(self, bookmark_formats = set(['json'])):
+    def export_bookmarks(self, bookmark_formats = set(['json']), directory = os.getcwd()):
         """Exports bookmarks in each of the formats specified"""
         # sanitize formats - these will be used directly
-        formats = set(['html', 'json', 'xml']) & bookmark_formats
-        map(self._download_bookmarks, formats)
+        formats = PinboardExporter._VALID_FORMATS & bookmark_formats
+        for f in formats: self._download_bookmarks(directory, f)
 
     def _connect(self):
         """Connects to the pinboard.in website."""
@@ -28,31 +29,54 @@ class PinboardExporter(object):
         self.site.open(PinboardExporter._LOGIN_PAGE, login_data)
         #TODO test if the login succeeded
 
-    def _download_bookmarks(self, bookmark_format):
+    def _download_bookmarks(self, directory, bookmark_format):
         """Downloads pinboard bookmarks in a given format"""
         export_page = PinboardExporter._EXPORT_BASE_PAGE + bookmark_format + '/'
         export_file = 'bookmarks.' + bookmark_format
-        with open(export_file, 'w') as output:
+        export_path = os.path.join(directory, export_file)
+        with open(export_path, 'w') as output:
             resp = self.site.open(export_page)
             output.writelines(resp)
 
     _LOGIN_PAGE = 'https://pinboard.in/auth/'
     _EXPORT_BASE_PAGE = 'http://pinboard.in/export/format:'
+    _VALID_FORMATS = set(['html', 'json', 'xml'])
 
 def usage():
-    print "\nUsage: pinboardExport.py -u|--username <username> -p|--password <password> [-f|--formats <formats>]\n"
+    print """
+    Usage: pinboardExport.py OPTIONS
+
+    OPTIONS:
+
+        -h | --help
+
+        -u <username> | --username <username>
+        
+        -p <password> | --password <password>
+
+        -f <formats>  | --formats <formats> 
+            A comma-separated list of formats to export. Defaults to json. 
+            Valid options are: json, html, xml.
+
+        -d | --directory <directory>
+            Defaults to current directory.
+
+    """
 
 def options(args):
     """Processes command line arguments"""
 
     try:
-        opts, args = getopt.getopt(args, 'u:p:f:',['username=','password=','formats='])
+        opts, args = getopt.getopt(args, 'u:p:f:hd:',['username=','password=','formats=','help','directory='])
     except getopt.GetoptError, e:
         print str(e)
         usage()
         sys.exit(2)
 
-    username = password = formats = None
+    # Defaults
+    username = password = None
+    formats = set(['json'])
+    directory = os.getcwd()
     
     for o,v in opts:
         if o in ('-u', '--username'):
@@ -60,7 +84,16 @@ def options(args):
         elif o in ('-p', '--password'):
             password = v
         elif o in ('-f', '--formats'):
-            formats = set(v.split(','))
+            formats = set(map(str.strip, v.split(',')))
+        elif o in ('-d', '--directory'):
+            if os.path.exists(v):
+                directory = v
+            else:
+                print "Destination directory does not exist."
+                sys.exit(2)
+        elif o in ('-h', '--help'):
+            usage()
+            sys.exit(2)
         else:
             usage()
             sys.exit(2)
@@ -70,13 +103,10 @@ def options(args):
         username = raw_input('Username: ')
     if not password:
         password = getpass.getpass('Password: ')
-    if not formats:
-        formats = raw_input('Formats: ')
-        formats = set(formats.split(','))
 
-    return (username, password, formats)
+    return (username, password, formats, directory)
 
 if __name__ == '__main__':
-    username, password, formats = options(sys.argv[1:])
+    username, password, formats, directory = options(sys.argv[1:])
     exporter = PinboardExporter(username,password)
-    exporter.export_bookmarks(formats)
+    exporter.export_bookmarks(formats, directory)
