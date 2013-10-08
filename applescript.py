c2a3e8806d61b53c278a9c8f20ca7e1df77554cd
Ed Gauthier <ed@egauthier.net>
2013-10-08 19:29:53 -0400

    Updated indentation and support unicode characters on input piped to osascript command.

--- applescript.py
+++ applescript.py
@@ -3,16 +3,18 @@
 import subprocess
 
 def asrun(ascript):
-  "Run the given AppleScript and return the standard output and error."
+    "Run the given AppleScript and return the standard output and error."
 
-  osa = subprocess.Popen(['osascript', '-'],
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-  return osa.communicate(ascript)[0]
+    ascript = ascript.encode('utf-8')
+    osa = subprocess.Popen(['osascript', '-'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
+    return osa.communicate(ascript)[0]
 
 def asquote(astr):
-  "Return the AppleScript equivalent of the given string."
-  
-  astr = astr.replace('"', '" & quote & "')
-  return '"{}"'.format(astr)
+    "Return the AppleScript equivalent of the given string."
+
+    # TODO Not sure if this handles backslash characters correctly
+    astr = astr.replace('"', '" & quote & "')
+    return u'"{}"'.format(astr)
 
