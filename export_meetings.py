b32bdcc7e8fcbb39eddfac61970ce064e1033837
Ed Gauthier <ed@egauthier.net>
2013-12-28 12:07:06 -0500

    Updated script for exporting meetings

--- export_meetings.py
+++ export_meetings.py
@@ -2,19 +2,24 @@
 
 from datetime import datetime, timedelta
 import subprocess
-from pprint import pprint
+import os
 
 ICAL_BUDDY = '/usr/local/bin/icalBuddy'
 CALENDARS = ['Work (Ed)']
+EXPORT_DIR = '/Users/plex/Dropbox/Public/Meetings'
 
 # /usr/local/bin/icalBuddy --includeCals "Work (Ed)" --noCalendarNames --includeEventProps title,datetime --bullet "" --propertySeparators "*||*" -nrd eventsToday
 
 def main():
     now = datetime.now()
     yesterday = get_events_for_dates(now-timedelta(days=1))
+    export_events(yesterday, 'yesterday')
     today = get_events_for_dates(now)
+    export_events(today, 'today')
     tomorrow = get_events_for_dates(now+timedelta(days=1))
+    export_events(tomorrow, 'tomorrow')
     three_days = get_events_for_dates(now-timedelta(days=1), now+timedelta(days=1))
+    export_events(three_days, 'three_days')
 
 def get_events_for_dates(start_date, end_date = None):
     calendar_list = ','.join(['{0}'.format(c) for c in CALENDARS])
@@ -30,7 +35,10 @@ def get_events_for_dates(start_date, end_date = None):
     cmd.extend(['--includeEventProps', 'title,datetime'])
     cmd.extend(['--bullet', ''])
     cmd.extend(['--propertySeparators', '*||*'])
-    cmd.append('-nrd')
+    cmd.append('--excludeAllDayEvents')
+    cmd.append('--noRelativeDates')
+    cmd.append('--excludeEndDates')
+    cmd.extend(['--dateFormat', '%Y-%m-%d'])
     cmd.extend(event_dates_string.split())
     
     event_list = subprocess.check_output(cmd)
@@ -43,6 +51,12 @@ def parse_event_list(event_list):
         events.append({'name': event_name, 'datetime': event_datetime})
     return events
 
+def export_events(events, name):
+    filename = os.path.join(EXPORT_DIR, '{0}.txt'.format(name))
+    with open(filename, 'w') as f:
+        for event in events:
+            f.write('{0}\t{0}||{1}\n'.format(event['name'], event['datetime']))
+        pass
 
 if __name__ == '__main__':
     main()
